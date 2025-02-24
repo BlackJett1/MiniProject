fb601a8d273ead6a39707165ba6b5f4fedbcf729
BT_1N3_34 <student@bths.edu>
2025-02-24 13:18:25 -0500

    instant vairables in movie class

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,9 @@
 public class Movie {
+    private String movie = " ";
+    private  String cast = " ";
+    private String director = " ";
+    private String overview = " ";
+    private int runtime = 0;
+    private  double  userRating = 0.0;
+
 }
