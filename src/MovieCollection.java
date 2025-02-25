816a609d1c0dd417cadf05e2f3c83be039a34b8c
BT_1N3_33 <SHUTUP@bths.edu>
2025-02-25 12:54:31 -0500

    added instance variables in MovieCollection

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,2 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 public class MovieCollection {
+    private Scanner scan = new Scanner(System.in);
+    private ArrayList<String> list = new ArrayList<String>();
 }
