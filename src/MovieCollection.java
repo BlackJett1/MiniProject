dcd8e673cc27114ec847eac2e72a3a9f06874aac
BT_1N3_33 <SHUTUP@bths.edu>
2025-02-25 13:14:17 -0500

    fixed an issue

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class MovieCollection {
     private Scanner scan = new Scanner(System.in);
-    private ArrayList<String> list = new ArrayList<String>();
+    private ArrayList<Movie> list = new ArrayList<Movie>();
 
     public MovieCollection() {
         list = new ArrayList<>();
