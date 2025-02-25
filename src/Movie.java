d6a6d40b7a65793a247d9404f87a3043eb4a91f3
BT_1N3_34 <abc@bths.edu>
2025-02-25 12:46:46 -0500

    movie class 1

--- src/Movie.java
+++ src/Movie.java
@@ -1,9 +1,23 @@
 public class Movie {
-    private String movie = " ";
-    private  String cast = " ";
-    private String director = " ";
-    private String overview = " ";
-    private int runtime = 0;
-    private  double  userRating = 0.0;
+    private String movie;
+    private  String cast;
+    private String director;
+    private String overview;
+    private int runtime;
+    private  double  userRating;
+
+    public Movie(String movie, String cast,String director, String overview, int runtime, double userRating ){
+        this.movie = movie;
+        this.cast = cast;
+        this.director =director;
+        this.overview = overview;
+        this.runtime = runtime;
+        this.userRating = userRating;
+
+    }
+    public String getMovie() {
+        return  movie;
+    }
+    public String
 
 }
