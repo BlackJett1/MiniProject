de4abc9f8d3f250bed021b32f5eca456e966bd18
BT_1N3_33 <SHUTUP@bths.edu>
2025-02-25 13:17:00 -0500

    3b start

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -14,6 +14,39 @@ public class MovieCollection {
         displayMenu();
     }
 
+    private void displayMenu(){
+        System.out.println("Welcome to the movie collection!");
+        String menuOption = "";
+
+        while (!menuOption.equals("q")) {
+            System.out.println("------------ Main Menu ----------");
+            System.out.println("- search (t)itles");
+            System.out.println("- search (c)ast");
+            System.out.println("- (q)uit");
+            System.out.print("Enter choice: ");
+            menuOption = scan.nextLine();
+
+            if (menuOption.equals("t")) {
+                searchTitles();
+            } else if (menuOption.equals("c")) {
+                searchCast();
+            } else if (menuOption.equals("q")) {
+                System.out.println("Goodbye!");
+            } else {
+                System.out.println("Invalid choice!");
+            }
+        }
+
+    }
+
+    private void searchTitles(){
+
+    }
+
+    private void searchCast(){
+
+    }
+
     private void importData() {
         try (BufferedReader br = new BufferedReader(new FileReader("movies_data.csv"))) {
             String line;
