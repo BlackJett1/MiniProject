22be845766a09623a1ee13b04bee4ee5bcdff831
BT_1N3_34 <abc@bths.edu>
2025-02-25 12:54:45 -0500

    movie class 2

--- src/MovieCollectionRunner.java
+++ src/MovieCollectionRunner.java
@@ -1,6 +1,7 @@
-import  java.util.Scanner;
+import java.util.Scanner;
 public class MovieCollectionRunner {
     public static void main(String[] args) {
+
         MovieCollection collection = new MovieCollection();
     }
 }
