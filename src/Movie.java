2879f58831906951dba6f368bd8eef45c1c1f881
BT_1N3_34 <abc@bths.edu>
2025-02-25 13:07:28 -0500

    moviecollection  class 2

--- src/Movie.java
+++ src/Movie.java
@@ -1,12 +1,12 @@
 public class Movie {
     private String movie;
-    private  String cast;
+    private  String[] cast;
     private String director;
     private String overview;
     private int runtime;
     private  double  userRating;
 
-    public Movie(String movie, String cast,String director, String overview, int runtime, double userRating ){
+    public Movie(String movie, String[] cast,String director, String overview, int runtime, double userRating ){
         this.movie = movie;
         this.cast = cast;
         this.director =director;
@@ -18,7 +18,7 @@ public class Movie {
     public String getMovie() {
         return  movie;
     }
-    public String getCast(){
+    public String[] getCast(){
         return cast;
     }
     public String getDirector(){
--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,7 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 public class MovieCollection {
     private Scanner scan = new Scanner(System.in);
     private ArrayList<String> list = new ArrayList<String>();
+
+    public MovieCollection() {
+        list = new ArrayList<>();
+        importData();
+        displayMenu();
+    }
+
+    private void importData() {
+        try (BufferedReader br = new BufferedReader(new FileReader("movies_data.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+                String movie = fields[0];
+                int runtime = Integer.parseInt(fields[1]);
+                String director = fields[2];
+                String[] cast = fields[3].split("\\|");
+                String overview = fields[4];
+                double userRating = Double.parseDouble(fields[5]);
+                Movie movie1 = new Movie(movie, cast, director, overview, userRating);
+                list.add(movie1);
+            }
+        }
+    }
 }
